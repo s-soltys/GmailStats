e78d0dcd096001d9aa63848fccf0eddd3b1d3794
Szymon Sołtysiak <szymon.soltysiak@gmail.com>
2016-04-23 21:25:35 +0200

    Implement letter histogram for the latest 100 message snippets.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,128 +11,41 @@ angular.module('gmailHistogramApp')
   .controller('MainCtrl', ['$scope', 'gmailApi', '$q', function ($scope, gmailApi, $q) {
     var main = this;
     main.messages = [];
+    main.chartData = null;
+    main.waiting = false;
 
     this.authAndLoadData = function loadGmailData() {
+      main.waiting = true;
       gmailApi.authorize().then(this.loadMessages);
     };
 
     this.loadMessages = function loadMessages() {
       gmailApi.getMessageList().then(function messagesReceived(messages) {
-        var loadMessageByIdPromises = messages.map(function(m) { return gmailApi.getMessageById(m.id) });
-        
+        var loadMessageByIdPromises = messages.map(function (m) { return gmailApi.getMessageById(m.id) });
+
         $q.all(loadMessageByIdPromises).then(function (results) {
-            main.messages = results.map(function(r) { return { id: r.id, snippet: r.snippet } });
-            main.updateData();
+          main.messages = results.map(function (r) { return { id: r.id, snippet: r.snippet } });
+          main.updateData();
+          main.waiting = false;
         });
       });
     };
-    
+
     this.updateData = function updateData() {
       var items = _
         .chain(main.messages)
         .map(function (msg) { return msg.snippet; })
-        .flatMap(function (str) { return str.split('') })
-        .map(function (str) { return str.toLowerCase(); })
-        .filter(function (char) { return char.match(/[a-z]/i); })
+        .flatMap(function (s) { return s.split('') })
+        .map(function (s) { return s.toLowerCase(); })
+        .filter(function (s) { return s.match(/[a-z]/i); })
         .groupBy()
         .map(function (items, key) { return { "name": key, "size": items.length } })
         .value();
 
-      main.globData = {
+      main.chartData = {
         "name": "flare",
         "children": items
       };
     };
-
-    this.globData = {
-      "name": "flare",
-      "children": [
-        {
-          "name": "A",
-          "size": 100000
-        },
-        {
-          "name": "animate",
-          "children": [
-            { "name": "Easing", "size": 17010 }
-          ]
-        },
-        {
-          "name": "data",
-          "children": [
-            {
-              "name": "converters",
-              "children": [
-                { "name": "Converters", "size": 721 },
-                { "name": "DelimitedTextConverter", "size": 4294 },
-                { "name": "GraphMLConverter", "size": 9800 },
-                { "name": "IDataConverter", "size": 1314 },
-                { "name": "JSONConverter", "size": 2220 }
-              ]
-            },
-            { "name": "DataField", "size": 1759 },
-            { "name": "DataSchema", "size": 2165 },
-            { "name": "DataSet", "size": 586 },
-            { "name": "DataSource", "size": 3331 },
-            { "name": "DataTable", "size": 772 },
-            { "name": "DataUtil", "size": 3322 }
-          ]
-        },
-        {
-          "name": "display",
-          "children": [
-            { "name": "DirtySprite", "size": 8833 },
-            { "name": "LineSprite", "size": 1732 },
-            { "name": "RectSprite", "size": 3623 },
-            { "name": "TextSprite", "size": 10066 }
-          ]
-        },
-        {
-          "name": "util",
-          "children": [
-            { "name": "Arrays", "size": 8258 },
-            { "name": "Colors", "size": 10001 },
-            { "name": "Dates", "size": 8217 },
-            { "name": "Displays", "size": 12555 },
-            { "name": "Filter", "size": 2324 },
-            { "name": "Geometry", "size": 10993 },
-            {
-              "name": "heap",
-              "children": [
-                { "name": "FibonacciHeap", "size": 9354 },
-                { "name": "HeapNode", "size": 1233 }
-              ]
-            },
-            { "name": "IEvaluable", "size": 335 },
-            { "name": "IPredicate", "size": 383 },
-            { "name": "IValueProxy", "size": 874 },
-            {
-              "name": "math",
-              "children": [
-                { "name": "DenseMatrix", "size": 3165 },
-                { "name": "IMatrix", "size": 2815 },
-                { "name": "SparseMatrix", "size": 3366 }
-              ]
-            },
-            { "name": "Maths", "size": 17705 },
-            { "name": "Orientation", "size": 1486 },
-            {
-              "name": "palette",
-              "children": [
-                { "name": "ColorPalette", "size": 6367 },
-                { "name": "Palette", "size": 1229 },
-                { "name": "ShapePalette", "size": 2059 },
-                { "name": "SizePalette", "size": 2291 }
-              ]
-            },
-            { "name": "Property", "size": 5559 },
-            { "name": "Shapes", "size": 19118 },
-            { "name": "Sort", "size": 6887 },
-            { "name": "Stats", "size": 6557 },
-            { "name": "Strings", "size": 22026 }
-          ]
-        }
-      ]
-    };
-
+    
   }]);
--- app/scripts/directives/bubble-chart.directive.js
+++ app/scripts/directives/bubble-chart.directive.js
@@ -1,25 +1,20 @@
 'use strict';
 
-var drawModule = function (svg, inputData) {
-    var diameter = 500,
-        format = d3.format(",d"),
-        color = d3.scale.category20c();
+var drawModule = function (svg, diameter, inputData) {
+    var format = d3.format(",d");
+    var color = d3.scale.category20c();
 
     var bubble = d3.layout.pack()
         .sort(null)
         .size([diameter, diameter])
         .padding(1.5);
-
-    svg.attr("width", diameter)
-        .attr("height", diameter)
-        .attr("class", "bubble");
         
-    d3.select(self.frameElement).style("height", diameter + "px");
+    var flatData = bubble.nodes(flattenData(inputData)).filter(function (d) { return !d.children; });
 
-    var node = svg.selectAll(".node")
-        .data(bubble.nodes(classes(inputData))
-            .filter(function (d) { return !d.children; }))
-        .enter().append("g")
+    var node = svg.selectAll("g")
+        .data(flatData)
+        .enter()
+        .append("g")
         .attr("class", "node")
         .attr("transform", function (d) { return "translate(" + d.x + "," + d.y + ")"; });
 
@@ -33,9 +28,9 @@ var drawModule = function (svg, inputData) {
     node.append("text")
         .attr("dy", ".3em")
         .style("text-anchor", "middle")
-        .text(function (d) { return d.className.substring(0, d.r / 3); });
+        .text(function (d) { return d.className.toUpperCase(); });
     
-    function classes(root) {
+    function flattenData(root) {
         var classes = [];
         function recurse(name, node) {
             if (node.children) node.children.forEach(function (child) { recurse(node.name, child); });
@@ -55,7 +50,13 @@ angular
                 data: '='
             },
             link: function (scope, element, attrs) {
+                var diameter = 500;
                 var svg = d3.select(element[0]).append('svg');
+                svg.attr({
+                    'width': diameter,
+                    'height': diameter,
+                    'class': 'bubble'
+                });
 
                 $window.onresize = function () {
                     scope.$apply();
@@ -69,15 +70,13 @@ angular
                         scope.render(scope.data);
                     });
                     
-                scope.$watch(
-                    'data',
+                scope.$watch('data',
                     function () {
                         scope.render(scope.data);
                     });
 
                 scope.render = function (data) {
-                    console.log('render');
-                    drawModule(svg, data);
+                    drawModule(svg, diameter, data);
                 };
             }
         };
