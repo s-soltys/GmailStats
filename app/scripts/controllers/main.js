3ecac897d34412cf5e20467dc39135e3a102d3a6
Szymon Sołtysiak <szymon.soltysiak@gmail.com>
2016-04-24 13:23:46 +0200

    Move the gmail message loading code to a separate component.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -12,34 +12,17 @@ angular.module('gmailHistogramApp')
     var vm = this;
     vm.messages = [];
     vm.chartData = null;
-    vm.waiting = false;
-    
-    this.authAndLoadData = function loadGmailData() {
-      vm.waiting = true;
-      gmailApi.authorize().then(this.loadMessages);
-    };
 
-    this.loadMessages = function loadMessages() {
-      gmailApi.getMessageList().then(function messagesReceived(messages) {
-        var loadMessageByIdPromises = messages.map(function (m) { return gmailApi.getMessageById(m.id) });
-
-        $q.all(loadMessageByIdPromises).then(function (results) {
-          vm.messages = results.map(function (r) { return { id: r.id, snippet: r.snippet } });
-          vm.updateData();
-          vm.waiting = false;
-        });
-      });
-    };
-
-    this.updateData = function updateData() {
-      var msgArray = vm.messages.map(function(msg) { return msg.snippet; });
+    this.onLoadMessages = function onLoadMessages(messages) {
+      vm.messages = messages;
+      var msgArray = messages.map(function (msg) { return msg.snippet; });
       var histogram = letterHistogram.create(msgArray);
-      var chartChildren = _.map(histogram, function(count, letter) { return { "name": letter, "size": count }; });
+      var chartChildren = _.map(histogram, function (count, letter) { return { "name": letter, "size": count }; });
 
       vm.chartData = {
         "name": "flare",
         "children": chartChildren
       };
     };
-    
+
   }]);
---
+++ app/scripts/gmail-message-loader/gmail-message-loader.component.js
@@ -0,0 +1,44 @@
+(function () {
+    'use strict';
+
+    var messageLoaderController = ['$scope', '$element', '$attrs', '$q', 'gmailApi', function messageLoaderController($scope, $element, $attrs, $q, gmailApi) {
+        var ctrl = this;
+        ctrl.waiting = false;
+
+        ctrl.loadMockData = function loadMockData() {
+            var msgs = [
+                { id: 13, snippet: 'dfsdfsdfsdfs' },
+                { id: 10, snippet: 'aaaaaaaaaaaaaaaa' }
+            ];
+            
+            ctrl.onLoadMessages({ messages: msgs });
+        };
+        
+        ctrl.authAndLoadData = function loadGmailData() {
+            ctrl.waiting = true;
+            gmailApi.authorize().then(this.loadMessages);
+        };
+
+        ctrl.loadMessages = function loadMessages() {
+            gmailApi.getMessageList().then(function messagesReceived(messages) {
+                var loadMessageByIdPromises = messages.map(function (m) { return gmailApi.getMessageById(m.id) });
+
+                $q.all(loadMessageByIdPromises).then(function (results) {
+                    ctrl.waiting = false;
+                    var messages = results.map(function (r) { return { id: r.id, snippet: r.snippet } });
+                    ctrl.onLoadMessages({ messages: messages });
+                });
+            });
+        };
+    }];
+
+    angular
+        .module('gmailHistogramApp')
+        .component('gmailMessageLoader', {
+            templateUrl: 'scripts/gmail-message-loader/gmail-message-loader.html',
+            controller: messageLoaderController,
+            bindings: {
+                onLoadMessages: '&'
+            }
+        });
+} ());
