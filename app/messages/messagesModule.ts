f76abde5bf5526c6d66decff4dacb52e48400949
Szymon Sołtysiak <szymon.soltysiak@gmail.com>
2016-05-10 19:15:46 +0200

    Convert the message loader component to typescript.

--- app/messages/messagesModule.ts
+++ app/messages/messagesModule.ts
@@ -3,12 +3,12 @@ import {LetterHistogramService} from './services/letterHistogramService';
 import {GmailApiService} from './services/gmailApiService';
 import {MessageListComponent} from './messageList';
 import {MessageViewComponent} from './messagesViewComponent';
+import {MessageLoaderComponent} from './messageLoaderComponent';
         
 angular
 .module('gs.messages', [])
 .component('messageList', new MessageListComponent())
 .component('messagesView', new MessageViewComponent())
+.component('messageLoader', new MessageLoaderComponent())
 .service('letterHistogram', LetterHistogramService)
 .service('gmailApi', GmailApiService);
-
-import './message-loader/message-loader.component';
